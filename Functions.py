0c880c6a7b80c1fefe96736cda0d6b4ffe0185a7
RyanTibbo87 <164216183+RyanTibbo87@users.noreply.github.com>
2024-03-20 13:49:27 -0230

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,198 @@
+# funkytown functions
+def isValidName(name):
+    ALLOWED_CHAR_SET = set("AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz-. '1234567890")
+    return set(name).issubset(ALLOWED_CHAR_SET)
+
+def isValidNum(name):
+    ALLOWED_NUM_SET = set("1234567890-")
+    return set(name).issubset(ALLOWED_NUM_SET)
+
+
+def customerInfo():
+    while True:
+        # Customer first name
+        custFirstName = input("Enter the customers first name (Type 'End' to end program.): ").title()
+        if custFirstName == "End":
+            exit()
+        elif isValidName(custFirstName):
+            break
+        print("Error - Please only use letters, numbers, hyphens, apostrophes, and/or periods.")
+    
+    while True:
+        # Customer last name
+        custLastName = input("Enter the customers last name: ").title()
+        if isValidName(custLastName):
+            break
+        print("Error - Please only use letters, numbers, hyphens, apostrophes, and/or periods.")
+
+    while True:
+        # Customer address
+        custStrAdd = input("Enter the customers street address: ")
+        if isValidName(custStrAdd):
+            break
+        print("Error - Please only use letters, numbers, hyphens, apostrophes, and/or periods.")
+
+    while True:
+        # Customer city
+        custCity = input("Enter the customer city: ").title()
+        if isValidName(custCity):
+            break
+        print("Error - Please only use letters, numbers, hyphens, apostrophes, and/or periods.")
+
+    # Province List
+    provLst = ["NL", "NS", "NB", "PE", "PQ", "ON", "MB", "AB", "BC", "NT", "YT", "SK", "NV"]
+    while True:
+        # Customer province
+        prov = input("Enter the customer province (XX): ")
+        if len(prov) != 2:
+            print("Error - Must be two characters only")
+        elif isValidName(prov):
+            break
+        
+        elif prov not in provLst:
+            print("Error - Invalid Province")
+        else:
+            break
+
+    while True:
+        # Customer postal code
+        postCode = input("Enter the customers postal code: ")
+        if isValidName(postCode):
+            break
+        print("Error - Please only use letters, numbers, hyphens, apostrophes, and/or periods.")
+
+    while True:
+        # Customer phone number
+        phoneNum = input("Enter the customers phone number (###-###-####): ")
+        if isValidNum(phoneNum):
+            break
+        print("Error - Please only use numbers.")
+
+    # Returns
+    return custFirstName.title(), custLastName.title(), custStrAdd, custCity.title(), prov.upper(), postCode, phoneNum
+
+def vehicleInfo():
+    while True:
+        # Number of cars being insured
+        numCars = input("Enter the amount of cars to be insured (Type End to end program). ")
+        if numCars == "End":
+            exit()
+        elif numCars.isdigit():
+            numCars = int(numCars)
+            break
+        print("Error - Please enter a valid number.")
+
+    while True:
+        # Extra Liability Coverage
+        extraLiab = input("Does the customer want Extra Liability Coverage? (Y for yes / N for no): ").upper()
+        if extraLiab in ["Y", "N"]:
+            break
+        print("Please enter Y or N.")
+        
+    if extraLiab == "Y":
+        print("Customer has decided for Extra Liability Coverage!")
+    else:
+        print("Customer has decided not to have Extra Liability Coverage.")
+
+    while True:
+        # Optional Glass Coverage
+        glassCoverage = input("Does the customer want Optional Glass Coverage? (Y for yes / N for no): ").upper()
+        if glassCoverage in ["Y", "N"]:
+            break
+        print("Please enter Y or N.")
+
+    if glassCoverage == "Y":
+        print("Customer has decided for Optional Glass Coverage!")
+    else:
+        print("Customer has decided not to have Optional Glass Coverage.")
+
+
+    while True:
+        # Optional Loaner Car
+        loanerCar = input("Does the customer want a Loaner Car? (Y for yes / N for no): ").upper()
+        if loanerCar in ["Y", "N"]:
+            break
+        print("Please enter Y or N.")
+
+    if loanerCar == "Y":
+        print("Customer has decided for a Loaner Car!")
+    else:
+        print("Customer has decided not to have a Loaner Car.")
+
+        # Returns
+    return numCars, extraLiab.upper(), glassCoverage.upper(), loanerCar.upper()
+
+
+def paymentInfo():
+    downPayment = 0
+    paymentMethodLst = ["Full", "Monthly", "Down Payment"]
+    while True:
+        # Paying in monthly or full amount
+        paymentMethod = input("How would the customer like to pay? (Full, Monthly, or Down payment.): ").title()
+        if paymentMethod not in paymentMethodLst:
+            print("Error - Invalid payment type.")
+        elif paymentMethod == "Down Payment":
+            while True:
+                downPaymentStr = input("How much would the customer like to pay as a down payment? ")
+                try:
+                    downPayment = float(downPaymentStr)
+                    break
+                except ValueError:
+                    print("Error - Please enter a numerical value.")
+            break
+        else:
+            break
+    return paymentMethod, downPayment
+
+
+def claimInfo():
+    while True:
+        # Claim number
+        claimNum = input("Enter the claim number. (Type End to end program) ")
+        if claimNum == "End":
+            exit()
+        elif claimNum.isdigit():
+            claimNum = int(claimNum)
+            break
+        print("Error - Please enter a numeric value.")
+
+    while True:
+        # Claim date
+        claimDate = input("Please enter the claim date (YYYY-MM-DD): ")
+        if isValidNum(claimDate):
+            break
+        print("Error - Please only use numbers.")
+
+  
+    # Returns
+    return claimNum, claimDate,
+
+def prevClaimInfo():
+    prevClaimLst = []
+
+    while True:
+        previousClaim = input("Does this customer have any previous claims? (Y for yes / N for no): ").upper()
+        if previousClaim == "N":
+            break
+        elif previousClaim != "Y":
+            print("Please enter Y or N.")
+            continue
+
+        previousClaimNumber = input("Please enter the previous claim number: ")
+        if not isValidNum(previousClaimNumber):
+            print("Error - Please only use numbers.")
+            continue
+
+        previousClaimDate = input("Please enter the previous claim date (YYYY-MM-DD): ")
+        if not isValidNum(previousClaimDate):
+            print("Error - Please only use numbers.")
+            continue
+
+        previousClaimAmount = input("Please enter the previous claim amount: ")
+        if not isValidNum(previousClaimAmount):
+            print("Error - Please only use numbers.")
+            continue
+
+        prevClaimLst.append((previousClaimNumber, previousClaimDate, previousClaimAmount))
+
+    return prevClaimLst
---
+++ QAP 4.py
@@ -0,0 +1,177 @@
+# Description: QAP# 4 - One stop Insruance Company
+# Author: Brandon Shea
+# Date(s): March 14th 2024 - March 20 2024
+
+# Define required libraries.
+import FormatValues as FV
+import datetime
+import sys
+import time
+import Functions as FN
+
+
+while True:
+    f = open("Default.dat", "r")
+    POLICY_NUM = int(f.readline())
+    BASIC_PREM = float(f.readline())
+    ADD_DISCOUNT = float(f.readline())
+    EXTRA_LIAB_COV = float(f.readline())
+    GLASS_COV = float(f.readline())
+    LOANER_CAR = float(f.readline())
+    HST_RATE = float(f.readline())
+    PROC_FEE = float(f.readline())
+    f.close()
+
+    curDate = datetime.datetime.now()
+
+    # Define program functions.
+
+    # Main program starts here.
+        
+    # Gather user inputs.
+    
+        # Perform required calculations and initialize variables.
+    finalCost = 0
+    while True:
+        customerData = FN.customerInfo()
+        insuranceInformation = FN.vehicleInfo()
+        paymentInformation = FN.paymentInfo()
+        claimInformation = FN.claimInfo()
+        previousClaimInformation = FN.prevClaimInfo()
+
+        custFirstName, custLastName, custStrAdd, custCity, prov, postCode, phoneNum   = customerData
+        numCars, extraLiab, glassCoverage, loanerCar = insuranceInformation
+        paymentMethod, downPayment = paymentInformation
+        claimNum, claimDate, = claimInformation
+        prevClaimLst = previousClaimInformation
+
+        if numCars == 1:
+            insurancePremium = BASIC_PREM
+        elif numCars > 1:
+            insurancePremium = BASIC_PREM * ADD_DISCOUNT
+
+        if extraLiab == "Y":
+            extraLiabilityCost = EXTRA_LIAB_COV
+        else:
+            extraLiabilityCost = 0
+
+        if glassCoverage == "Y":
+            glassCoverageCost = GLASS_COV
+        else:
+            glassCoverageCost = 0
+
+        if loanerCar == "Y":
+            loanerCarCost = LOANER_CAR
+        else:
+            loanerCarCost = 0
+
+        totalExtraCost = extraLiabilityCost + glassCoverageCost + loanerCarCost
+
+        totalInsurancePremium = BASIC_PREM + totalExtraCost
+
+        HST = totalInsurancePremium * HST_RATE
+
+        finalCost = totalInsurancePremium + HST
+
+        if paymentMethod == "Down Payment":
+            monthlyPayment = (finalCost - downPayment + PROC_FEE) / 8
+        else:
+            monthlyPayment = (finalCost + PROC_FEE) / 8
+
+
+    
+
+
+        
+        customerName = custFirstName + " " + custLastName
+        invoiceDate = datetime.date.today()
+
+        
+        claimDate = datetime.datetime.strptime(claimDate, "%Y-%m-%d").date()
+        
+        
+        nextMonth = invoiceDate.month + 1
+        year = invoiceDate.year
+
+        if nextMonth == 13:
+            nextMonth = 1
+            year += 1
+        
+        firstPaymentDate = datetime.date(year, nextMonth, 1)
+        
+
+
+
+
+
+
+
+        # Display results to the user.
+        print()
+        print("----------------------------------------------")
+        print("----------ONE STOP INSURANCE COMPANY----------")
+        print("----------------------------------------------")
+        print()
+        print("             CUSTOMER INFORMATION             ")
+        print()
+        print("----------------------------------------------")
+        print(f"Customer Name:     {customerName:<20s}       ")
+        print(f"Address:           {custStrAdd:<20s}         ")
+        print(f"City:              {custCity:<14s}           ")
+        print(f"Province:          {prov:<2s}                ")
+        print(f"Postal Code:       {postCode:<6s}            ")
+        print(f"Phone Number:      {phoneNum:<12s}           ")
+        print()
+        print("----------------------------------------------")
+        print()
+        print("                  ADDITIONS                   ")
+        print()
+        print("----------------------------------------------")
+        print(f"Number of Cars:    {numCars:<1d}             ")
+        print(f"Extra Liability:   {extraLiab:<1s}           ")
+        print(f"Glass Coverage:    {glassCoverage:<1s}       ")
+        print(f"Loaner Car:        {loanerCar:<1s}           ")
+        print(f"Payment Method:    {paymentMethod:<1s}       ")
+        print()
+        print("----------------------------------------------")
+        print()
+        print("              PAYMENT INFORMATION             ")
+        print() 
+        print("----------------------------------------------")
+        print(f"Down Payment Amount: {FV.FDollar2(downPayment):<8s}")
+        print(f"Insurance Premium:   {FV.FDollar2(totalInsurancePremium):<8s}")
+        print(f"Extra cost:          {FV.FDollar2(totalExtraCost):<8s}")
+        print(f"HST:                 {FV.FDollar2(HST):<8s}")
+        print(f"Total Cost:          {FV.FDollar2(finalCost):<8s}")
+        print()
+        print(f"Monthly Payment:     {FV.FDollar2(monthlyPayment):<8s}")
+        print()
+        print("----------------------------------------------")
+        print()
+        print("              POLICY INFORMATION              ")
+        print()
+        print("----------------------------------------------")
+        print(f"Policy Number:       {POLICY_NUM:<8d}        ")
+        print(f"Policy Date:         {FV.FDateS(curDate):<8s}")
+        print(f"First Payment Date:  {FV.FDateS(firstPaymentDate):<8s}")
+        print()
+        print("----------------------------------------------")
+        print()
+        print("               CLAIM INFORMATION              ")
+        print()
+        print("----------------------------------------------")
+        print(f"Claim Number:       {claimNum:<8d}          ")
+        print(f"Claim Date:         {FV.FDateS(claimDate):<8s}")
+        print(f"invoice Date:       {FV.FDateS(invoiceDate):<8s}")
+        print(f"first Payment Date: {FV.FDateS(firstPaymentDate):<8s}")
+        print()
+        print("----------------------------------------------")
+        print()
+        print("               PREVIOUS CLAIMS                ")
+        print()
+        print("----------------------------------------------")
+        print()
+        print(f"Claim #         Claim Date      Amount       ")
+        print("----------------------------------------------")
+        print()
+        print(prevClaimLst)
